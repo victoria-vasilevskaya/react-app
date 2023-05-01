37495bb1d6fe3556fb7f6e5a185e5260c7418f3d
kingofcutie <yanesobakaa@gmail.com>
2023-05-02 02:43:25 +0300

    4

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className={st.app}>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route path='/main' element={<Main />} />
       </Routes>
     </div>
